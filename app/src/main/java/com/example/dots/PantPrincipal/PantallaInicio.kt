bff201864adbabca3d211e5e314751d270bcccba
Estalin1412 <jose.munoz.d@uni.pe>
2024-10-03 11:59:38 -0600

    SubiendoFRomtend

--- app/src/main/java/com/example/dots/PantPrincipal/PantallaInicio.kt
+++ app/src/main/java/com/example/dots/PantPrincipal/PantallaInicio.kt
@@ -33,6 +33,15 @@ class PantallaInicio : AppCompatActivity() {
         setContentView(R.layout.activity_pantalla_inicio)
 
 
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        or View.SYSTEM_UI_FLAG_FULLSCREEN
+                        or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+
         InitComponentes();
         InitListenersComponents();
 
